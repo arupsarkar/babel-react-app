9b4d1d249059d8ca9d0d398703816f3e3e4a9637
arupsarkar <sarkar.arup@gmail.com>
2022-02-09 23:37:56 -0500

    Modified package and updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 export default function App () {
 
-    return(
-        <div>
-            react app component
-        </div>
-    )
+    const[data, setData] = useState('')
+    useEffect(() => {
+        console.log('---> useEffect Start')
+        let date_str = new Date()
+        //date_str = `${date_str.getFullYear()}-${date_str.getMonth() + 1}-${date_str.getDate()}`
+        setData(date_str.toString())
+        console.log('---> useEffect End')
+    },[data])
+
+        return(
+            <div>
+                {data} react app component
+            </div>
+        )
 }
 
 
