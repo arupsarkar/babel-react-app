abf4f2994e00abc0aa7397348e9ca30d14870661
arupsarkar <sarkar.arup@gmail.com>
2022-02-09 23:14:34 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App () {
+
+    return(
+        <div>
+            react app component
+        </div>
+    )
+}
+
+
+/*
+Reference Material
+https://dev.to/lschwall/make-a-react-app-with-webpack-babel-and-express-30n8
+*/
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
+
+// const Index = () => {
+//     return <div>WELCOME TO REACT APP!</div>;
+// };
+// ReactDOM.render(<Index />,document.getElementById('app'));
+// check the index.html where div id element is mentioned
+ReactDOM.render(<App />,document.getElementById('root'));
